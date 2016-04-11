395b9cb31a64ce5db805d95608b6ff7f5520b634
marco <marcovcambronero@gmail.com>
2016-04-11 01:41:32 -0600

    Cambios

--- RunApp2/app/src/main/java/com/example/runapp/runapp/Modelo/GestorBD.java
+++ RunApp2/app/src/main/java/com/example/runapp/runapp/Modelo/GestorBD.java
@@ -4,49 +4,21 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-<<<<<<< HEAD:RunApp2/app/src/main/java/com/example/runapp/runapp/Modelo/SQLiteHelpe.java
-/**
- * Created by iknox27 on 31/03/16.
- */
-public class SQLiteHelpe extends SQLiteOpenHelper {
-=======
 
 public class GestorBD extends SQLiteOpenHelper {
->>>>>>> origin/master:RunApp2/app/src/main/java/com/example/runapp/runapp/Modelo/GestorBD.java
-
-    private static final int VERSIONBD = 1;
-    public static final String nombreBD = "baseCorre.db";
-
-<<<<<<< HEAD:RunApp2/app/src/main/java/com/example/runapp/runapp/Modelo/SQLiteHelpe.java
-    public static String cID = "ID";
-    public static String NombreUs = "Nombre";
-    public static String estaturaUs= "Estatura";
-    public static String PesoUs= "Peso";
-    public static String EdadUs= "Edad";
-    public static String usuario= "Usuario";
 
 
-
-
-
-    public SQLiteHelpe(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-=======
     /*public GestorBD(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, nombreBD, null, VERSIONBD);
     }*/
 
 
     public GestorBD(Context context) {
->>>>>>> origin/master:RunApp2/app/src/main/java/com/example/runapp/runapp/Modelo/GestorBD.java
         super(context, nombreBD, null, VERSIONBD);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-<<<<<<< HEAD:RunApp2/app/src/main/java/com/example/runapp/runapp/Modelo/SQLiteHelpe.java
-            db.execSQL("create table" +  usuario + " ( " + cID + "INETEGER PRIMARY KEY AUTOINCREMENT," + NombreUs + " VARCHAR, "
-            + estaturaUs + " NUMERIC," + PesoUs + " NUMERIC," + EdadUs + "INTEGER);");
-=======
 
         String crea_Usuario =
                 "create table" + nombre_tabla_Us + " ( " +
@@ -83,11 +55,47 @@ public class GestorBD extends SQLiteOpenHelper {
  // hola
         // Abi se la come
         //hh
->>>>>>> origin/master:RunApp2/app/src/main/java/com/example/runapp/runapp/Modelo/GestorBD.java
     }
+
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+
+
+
+    // Base de Datos
+    private static final int VERSIONBD = 1;
+    public static final String nombreBD = "BD_RunApp.db";
+
+    // Tabla Usuario
+    public static String nombre_tabla_Us= "BD_RunApp_Usuario";
+    public static String cID_Us = "ID";
+    public static String nombreUs = "Nombre";
+    public static String apellidoUs = "Apellido";
+    public static String usernameUs = "Username";
+    public static String emailUs= "Email";
+    public static String edadUs= "Edad";
+    public static String pesoUs= "Peso";
+    public static String estaturaUs= "Estatura";
+
+    // Tabla Valoraciones
+    public static String nombre_tabla_Valoracion= "BD_RunApp_Valoracion";
+    public static String cID_Valoracion = "ID";
+    public static String cantidad_Estrellas = "Numero_Estrellas";
+    public static String calificacion_Valoracion = "Calificacion";
+
+    // Tabla Recorridos
+    public static String nombre_tabla_Recorrido= "BD_RunApp_Recorrido";
+    public static String cID_Recorrido = "ID";
+    public static String fecha_Recorrido = "Fecha_Recorrido";
+    public static String distancia_Recorrido = "Distancia_Recorrida";
+    public static String tiempo_Recorrido = "Tiempo_Recorrido";
+    public static String valoracion_Recorrido = "Valoracion_Recorrido";
+
+
 }
