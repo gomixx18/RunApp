e18adb059191631db20791caa5d7bbfa1fbf41a6
gomixx18 <bryanr9418@hotmail.com>
2016-04-13 17:16:03 -0600

    Map Activity
    Pone en la ubicacion actual

--- RunApp2/app/src/main/java/com/example/runapp/runapp/ResultadosActivity.java
+++ RunApp2/app/src/main/java/com/example/runapp/runapp/ResultadosActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ResultadosActivity extends AppCompatActivity {
 
@@ -18,5 +20,20 @@ public class ResultadosActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        findViewById(R.id.btnImgMapa).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultadosActivity.this, mapaRecorrido.class));
+            }
+        });
+
+        // alambramos el boton
+
+       // ImageButton MiBoton = (ImageButton) findViewById(R.id.btnImgMapa);
+
+        //Programamos el evento onclick
+
+       //
     }
 }
---
+++ RunApp2/app/src/main/java/com/example/runapp/runapp/mapaRecorrido.java
@@ -0,0 +1,79 @@
+package com.example.runapp.runapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapaRecorrido extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa_recorrido);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapa = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapa.getMapAsync(this);
+        mMap = mapa.getMap();
+
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        mMap.setMyLocationEnabled(true);
+
+        Criteria criteria = new Criteria();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        double lat =  location.getLatitude();
+        double lng = location.getLongitude();
+
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate ubicacion=
+                CameraUpdateFactory.newLatLng(coordenadas);
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(18);
+
+        mMap.moveCamera(ubicacion);
+        mMap.animateCamera(zoom);
+    }
+}
