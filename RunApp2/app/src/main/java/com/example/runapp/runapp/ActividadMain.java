81d47e3e5b2077e43f3974af77535581859d70ac
marco <marcovcambronero@gmail.com>
2016-05-29 03:57:59 -0600

    validarRegistro

--- RunApp2/app/src/main/java/com/example/runapp/runapp/ActividadMain.java
+++ RunApp2/app/src/main/java/com/example/runapp/runapp/ActividadMain.java
@@ -5,16 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ActividadMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_main);
+
+        // En el caso de que esté en RegistroFit y oprime "Anterior",
+        // se vuelven a cargar los datos (nombre, apellido, etc.) que digitó antes.
+        if(!(nombre.equals("")) && !(apellido.equals("")) &&
+                !(username.equals("")) && !(email.equals(""))){
+
+            EditText textoName = (EditText)findViewById(R.id.editNombre);
+            textoName.setText(nombre);
+
+            EditText textoApellido = (EditText)findViewById(R.id.editApellido);
+            textoApellido.setText(apellido);
+
+            EditText textoUsername = (EditText)findViewById(R.id.editUser);
+            textoUsername.setText(username);
+
+            EditText textoEmail = (EditText)findViewById(R.id.editCorreo);
+            textoEmail.setText(email);
+        }
+
+
         findViewById(R.id.btnSiguiente).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                boolean datosCorrectos = true;
 
                 EditText textoName = (EditText)findViewById(R.id.editNombre);
                 nombre = textoName.getText().toString();
@@ -28,7 +55,56 @@ public class ActividadMain extends AppCompatActivity {
                 EditText textoEmail = (EditText)findViewById(R.id.editCorreo);
                 email = textoEmail.getText().toString();
 
-                startActivity(new Intent(ActividadMain.this, RegistroFit.class));
+                // =====================================================================
+                // Se validan que los datos estén completos:
+                if(nombre.equals("")){
+                    datosCorrectos = false;
+                    Toast.makeText(ActividadMain.this, "El nombre es un dato requerido",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                if(datosCorrectos){
+                    if(apellido.equals("")){
+                        datosCorrectos = false;
+                        Toast.makeText(ActividadMain.this, "El apellido es un dato requerido",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                if(datosCorrectos){
+                    if(username.equals("")){
+                        datosCorrectos = false;
+                        Toast.makeText(ActividadMain.this, "El nombre de usuario es un dato requerido",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+                if(datosCorrectos) {
+
+                    if(!(email.equals(""))) {
+
+                        // Verificar que el email esté bien escrito:
+                        // Se define el patrón que debe seguir usando una expresión regular:
+                        String patron_Email = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+                        Pattern patron = Pattern.compile(patron_Email);
+                        Matcher asociador = patron.matcher(email);
+
+                        if (asociador.matches()) {  // si el email es correcto...
+                            startActivity(new Intent(ActividadMain.this, RegistroFit.class));
+                        } else {
+                            Toast.makeText(ActividadMain.this, "Formato de correo electrónico incorrecto",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(ActividadMain.this, "El correo electrónico es un dato requerido",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
             }
         });
 
@@ -52,8 +128,8 @@ public class ActividadMain extends AppCompatActivity {
     }
 
 
-    private static String nombre;
-    private static String apellido;
-    private static String username;
-    private static String email;
+    private static String nombre = "";
+    private static String apellido = "";
+    private static String username = "";
+    private static String email = "";
 }
--- RunApp2/app/src/main/java/com/example/runapp/runapp/RegistroFit.java
+++ RunApp2/app/src/main/java/com/example/runapp/runapp/RegistroFit.java
@@ -16,55 +16,131 @@ public class RegistroFit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_fit);
+
+        // En el caso de que digite datos, se devuelva a ActividadMain y cuando quiera
+        // volver a RegistroFit, debe estar edad, peso y estatura como los dejó.
+
+        if(!(edad.equals(""))) {
+            EditText textoEdad = (EditText)findViewById(R.id.editEdad);
+            textoEdad.setText(edad);
+        }
+
+
+        if(!(peso.equals(""))) {
+            EditText textoPeso = (EditText) findViewById(R.id.editPeso);
+            textoPeso.setText(peso);
+        }
+
+
+        if(!(estatura.equals(""))) {
+            EditText textoEstatura = (EditText) findViewById(R.id.editEstatura);
+            textoEstatura.setText(estatura);
+        }
+
+
+
+
         findViewById(R.id.btnAnterior).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                EditText textoEdad = (EditText) findViewById(R.id.editEdad);
+                edad = textoEdad.getText().toString();
+
+                EditText textoPeso = (EditText) findViewById(R.id.editPeso);
+                peso = textoPeso.getText().toString();
+
+                EditText textoEstatura = (EditText) findViewById(R.id.editEstatura);
+                estatura = textoEstatura.getText().toString();
+
                 startActivity(new Intent(RegistroFit.this, ActividadMain.class));
             }
         });
+
         findViewById(R.id.btnRegistrar).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String age, pes, estatur;
+                boolean sonNumeros = true;
+                int edadTemporal = 0;
+                float pesoTemporal = 0, estaturaTemporal = 0;
 
-                EditText textoEdad = (EditText)findViewById(R.id.editEdad);
-                age = textoEdad.getText().toString();
-                edad = Integer.parseInt(age);
+                EditText textoEdad = (EditText) findViewById(R.id.editEdad);
+                edad = textoEdad.getText().toString();
 
-                EditText textoPeso = (EditText)findViewById(R.id.editPeso);
-                pes = textoPeso.getText().toString();
-                peso = Float.parseFloat(pes);
+                EditText textoPeso = (EditText) findViewById(R.id.editPeso);
+                peso = textoPeso.getText().toString();
 
-                EditText textoEstatura = (EditText)findViewById(R.id.editEstatura);
-                estatur = textoEstatura.getText().toString();
-                estatura = Float.parseFloat(estatur);
+                EditText textoEstatura = (EditText) findViewById(R.id.editEstatura);
+                estatura = textoEstatura.getText().toString();
 
 
-                saveUser(new Usuario(
-                        ActividadMain.getNombre(),
-                        ActividadMain.getApellido(),
-                        ActividadMain.getUsername(),
-                        ActividadMain.getEmail(),
-                        edad,
-                        peso,
-                        estatura));
+                // En cada try-catch se va a comprobar si es un número
+                // lo que se digitó.
 
-                startActivity(new Intent(RegistroFit.this, incioDrawer.class));
+                try {
+                    Integer.parseInt(edad);
+                    edadTemporal = Integer.parseInt(edad);
+
+                } catch (NumberFormatException exception) {
+                    sonNumeros = false;
+                    Toast.makeText(RegistroFit.this, "La edad debe ser un dato numérico",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+
+                if (sonNumeros) {
+                    try {
+                        Float.parseFloat(peso);
+                        pesoTemporal = Float.parseFloat(peso);
+
+                    } catch (NumberFormatException exception) {
+                        sonNumeros = false;
+                        Toast.makeText(RegistroFit.this, "El peso debe ser un dato numérico",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+                if (sonNumeros) {
+                    try {
+                        Float.parseFloat(estatura);
+                        estaturaTemporal = Float.parseFloat(estatura);
+
+                    } catch (NumberFormatException exception) {
+                        sonNumeros = false;
+                        Toast.makeText(RegistroFit.this, "La estatura debe ser un dato numérico",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+                if (sonNumeros) {
+                    saveUser(new Usuario(
+                            ActividadMain.getNombre(),
+                            ActividadMain.getApellido(),
+                            ActividadMain.getUsername(),
+                            ActividadMain.getEmail(),
+                            edadTemporal,
+                            pesoTemporal,
+                            estaturaTemporal));
+
+                    startActivity(new Intent(RegistroFit.this, incioDrawer.class));
+                }
             }
         });
     }
 
 
-    public static int getEdad() {
+    public static String getEdad() {
         return edad;
     }
 
-    public static float getPeso() {
+    public static String getPeso() {
         return peso;
     }
 
-    public static float getEstatura() {
+    public static String getEstatura() {
         return estatura;
     }
 
@@ -75,7 +151,7 @@ public class RegistroFit extends AppCompatActivity {
         boolean exito = gestorBD.insertarUsuario(user);
 
         if(exito){
-            Toast.makeText(this, "Datos guardados exitosamente", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Registro completado exitosamente", Toast.LENGTH_LONG).show();
         }
         else
             Toast.makeText(this,gestorBD.getMensajeError(),Toast.LENGTH_LONG).show();
@@ -84,8 +160,8 @@ public class RegistroFit extends AppCompatActivity {
 
 
 
-    private static int edad;
-    private static float peso;
-    private static float estatura;
+    private static String edad = "";
+    private static String peso = "";
+    private static String estatura = "";
     public final GestorBD gestorBD = new GestorBD(this);
 }
