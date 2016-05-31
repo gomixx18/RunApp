8effb2ccc72c1d2394e1f76d0c4435c75d371a28
iknox27 <acam27@hotmail.com>
2016-05-31 16:51:24 -0600

    hjghjgh

--- RunApp2/app/src/main/java/com/example/runapp/runapp/mapaRecorrido.java
+++ RunApp2/app/src/main/java/com/example/runapp/runapp/mapaRecorrido.java
@@ -3,9 +3,11 @@ package com.example.runapp.runapp;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -18,6 +20,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class mapaRecorrido extends FragmentActivity implements OnMapReadyCallback {
 
@@ -32,61 +35,74 @@ public class mapaRecorrido extends FragmentActivity implements OnMapReadyCallbac
         mapa.getMapAsync(this);
 
 
-
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-
-        // Add a marker in Costa Rica and move the camera
-        LatLng sanJose = new LatLng(9.93, -84.10);
-        mMap.addMarker(new MarkerOptions().position(sanJose).title("Marker in Costa Rica"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sanJose));
-
-
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
+        pintar(mMap);
         //mMap.setMyLocationEnabled(true);
 
 
         //LatLng coordenadas = new LatLng(9.9354495,-84.1026813);
         //CameraUpdate ubicacion=
-                //CameraUpdateFactory.newLatLng(coordenadas);
+        //CameraUpdateFactory.newLatLng(coordenadas);
         //CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
 
         //Criteria criteria = new Criteria();
         //LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         //String provider = locationManager.getBestProvider(criteria, false);
-       // Location location = locationManager.getLastKnownLocation(provider);
+        // Location location = locationManager.getLastKnownLocation(provider);
 
         //double lat =  location.getLatitude();
         //double lng = location.getLongitude();
 
-       // LatLng coordenadas = new LatLng(lat, lng);
+        // LatLng coordenadas = new LatLng(lat, lng);
         //CameraUpdate ubicacion=
         //        CameraUpdateFactory.newLatLng(coordenadas);
-       // CameraUpdate zoom=CameraUpdateFactory.zoomTo(2);
+        // CameraUpdate zoom=CameraUpdateFactory.zoomTo(2);
 
 
-       // mMap.moveCamera(ubicacion);
+        // mMap.moveCamera(ubicacion);
         //mMap.animateCamera(zoom);
 
-       // Criteria criteria = new Criteria();
-       // LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-       // String provider = locationManager.getBestProvider(criteria, false);
-       // Location location = locationManager.getLastKnownLocation(provider);
+        Criteria criteria = new Criteria();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(criteria, false);
 
-        //double lat =  location.getLatitude();
-       // double lng = location.getLongitude();
+        Location location;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
-       // LatLng coordenadas = new LatLng(lat, lng);
-        //CameraUpdate ubicacion= CameraUpdateFactory.newLatLng(coordenadas);
-        //Ca/meraUpdate zoom=CameraUpdateFactory.zoomTo(18);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+                }else{
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+        }else{
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
 
-        //mMap.moveCamera(ubicacion);
-        //mMap.animateCamera(zoom);
+
+        double lat =  location.getLatitude();
+        double lng = location.getLongitude();
+
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate ubicacion= CameraUpdateFactory.newLatLng(coordenadas);
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(18);
+
+        mMap.moveCamera(ubicacion);
+        mMap.animateCamera(zoom);
+    }
+
+    public void pintar(GoogleMap googleMap){
+        for(int i = 0; i < CronoActivity.puntosLat.size() -1 ;i++) {
+            PolylineOptions line =
+                    new PolylineOptions().add(new LatLng(CronoActivity.puntosLat.get(i),CronoActivity.puntosLong.get(i)),
+                            new LatLng(CronoActivity.puntosLat.get(i + 1),CronoActivity.puntosLong.get(i +1 )))
+                            .width(5).color(Color.RED);
+
+            googleMap.addPolyline(line);
+        }
     }
 }
